6ba28144d901c7ab74c6581811e7ce3df7ee2043
hyungki Shin <tkaqkeldk@nate.com>
2021-12-04 14:52:36 +0900

    [api] line update

--- server/api/tv.ts
+++ server/api/tv.ts
@@ -1,6 +1,8 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import * as url from "url";
+
 export default async (req: IncomingMessage, res: ServerResponse) => {
+  
   const queryObject = url.parse(req.url as string, true).query;
   let data = { data: [{ data: "" }] };
   const { search } = queryObject;
